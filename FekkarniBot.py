ef95a80395b6a6ae53273f7b433a1a3e818b2fb9
Youness Nait oufkir <42207480+YounessNaitOufkir@users.noreply.github.com>
2026-06-19 20:03:09 +0100

    Change the model

--- FekkarniBot.py
+++ FekkarniBot.py
@@ -23,7 +23,7 @@ SHEET_NAME = os.getenv("SHEET_NAME")
 
 # Init Gemini
 genai.configure(api_key=GEMINI_API_KEY)
-ai_model = genai.GenerativeModel("gemini-2.5-flash")
+ai_model = genai.GenerativeModel("gemini-2.0-flash")
 
 # Connect to Google Sheets
 print("Connecting to Google Sheets...")
